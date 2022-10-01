93cba6af936c029ecd1ea41b6bc3899db01c6866
annony <ltn2057@protonmail.com>
2022-10-01 09:21:36 -0400

    functions for unit test

--- assignment.js
+++ assignment.js
@@ -2,6 +2,42 @@ function capitalize(s) {
   return s.toUpperCase()
 }
 
-console.log(capitalize("something"))
+function reverseString(s) {
+  return s.split("").reverse().join("")
+}
 
-export { capitalize }
+const calculator = {
+  add: (a, b) => {
+    return a + b
+  },
+  sub: (a, b) => {
+    return a - b
+  },
+  multiple: (a, b) => {
+    return a * b
+  },
+  divide: (a, b) => {
+    return a / b
+  },
+}
+
+function cipher(s) {
+  const arr = s.split("")
+  for (let i = 0; i < arr.length; i++) {
+    let newChar = String.fromCharCode(s.charCodeAt(i) + 1)
+    if (s.charCodeAt(i) + 1 > 122) newChar = "a"
+    arr[i] = newChar
+  }
+  return arr.join("")
+}
+
+function analyzeArray(array) {
+  let average =
+    array.reduce((previous, current) => previous + current) / array.length
+  let min = Math.min(...array)
+  let max = Math.max(...array)
+  let length = array.length
+  return { average, min, max, length }
+}
+
+export { capitalize, reverseString, calculator, cipher, analyzeArray }
--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,49 @@
-import capitalize from "./assignment"
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  cipher,
+  analyzeArray,
+} from "./assignment"
 
-test("to uppercase", () => {
+test("capitalize", () => {
   expect(capitalize("something")).toBe("SOMETHING")
+})
+
+test("reverseString", () => {
+  expect(reverseString("something")).toBe("gnihtemos")
+})
+
+test("calculator.add", () => {
+  expect(calculator.add(1, 2)).toBe(3)
+})
+
+test("calculator.sub", () => {
+  expect(calculator.sub(2, 1)).toBe(1)
+})
+
+test("calculator.multiple", () => {
+  expect(calculator.multiple(1, 2)).toBe(2)
+})
+
+test("calculator.divide", () => {
+  expect(calculator.divide(1, 2)).toBeCloseTo(1 / 2)
+})
+
+test("caesarCipher", () => {
+  expect(cipher("abc")).toBe("bcd")
+})
+
+test("caesarCipher", () => {
+  expect(cipher("xyz")).toBe("yza")
+})
+
+test("analyze", () => {
+  const object = {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  }
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual(object)
 })
